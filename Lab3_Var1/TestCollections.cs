03388286aef11a772b0f19f81516afb8209069b2
rchurianov <rchurianov@gmail.com>
2017-02-02 14:04:21 +0300

    TestCollections - added stub class

---
+++ Lab3_Var1/TestCollections.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Var1
+{
+    class TestCollections
+    {
+        //List<TKey>;
+        //List<string>;
+        //Dictionary<TKey, TValue>;
+        //Dictionary<string, TValue>;
+
+        public TestCollections()
+        {
+
+        }
+    }
+}
