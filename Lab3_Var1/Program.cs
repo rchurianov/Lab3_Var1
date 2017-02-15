b70e95a14a9339fd397eef6f1b88e711d98edc17
rchurianov <rchurianov@gmail.com>
2017-02-15 15:04:09 +0300

    Program - added foolproof input

--- Lab3_Var1/Program.cs
+++ Lab3_Var1/Program.cs
@@ -10,6 +10,15 @@ namespace Lab3_Var1
     {
         static void Main(string[] args)
         {
+            int input_number = 0;
+            while (true)
+            {
+                Console.WriteLine("Please, input the number of elements in collecions.\n" +
+                                  "The number should be a nonnegative integer.");
+                bool parse_result_success = Int32.TryParse(Console.ReadLine(), out input_number);
+                if (parse_result_success)
+                    break;
+            }
         }
     }
 }
