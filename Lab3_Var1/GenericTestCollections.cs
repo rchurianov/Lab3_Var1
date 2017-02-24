acb5fd6d7b4ec31ee12ce197a82680b1fd40a3a1
rchurianov <rchurianov@gmail.com>
2017-02-24 15:31:04 +0300

    GenericTestCollections - added public class

---
+++ Lab3_Var1/GenericTestCollections.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Var1
+{
+    public class GenericTestCollections<TKey, TValue>
+    {
+        private List<TKey> person_list;
+        private List<string> list_of_strings;
+        private Dictionary<TKey, TValue> person_student_dict;
+        private Dictionary<string, TValue> student_dict;
+
+        private GenerateElement<TKey, TValue> generate_element_method;
+    }
+}
