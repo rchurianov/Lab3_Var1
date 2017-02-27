0c048bc60ad932e0041a8298bcae2426ec7536ab
rchurianov <rchurianov@gmail.com>
2017-02-27 16:59:39 +0300

    GenericTestCollections - added constructor, ToString overload

--- Lab3_Var1/GenericTestCollections.cs
+++ Lab3_Var1/GenericTestCollections.cs
@@ -8,11 +8,61 @@ namespace Lab3_Var1
 {
     public class GenericTestCollections<TKey, TValue>
     {
-        private List<TKey> person_list;
+        private List<TKey> list_of_keys;
         private List<string> list_of_strings;
-        private Dictionary<TKey, TValue> person_student_dict;
-        private Dictionary<string, TValue> student_dict;
+        private Dictionary<TKey, TValue> key_value_dict;
+        private Dictionary<string, TValue> value_dict;
 
         private GenerateElement<TKey, TValue> generate_element_method;
+
+        /* Constuctor to create collections with specified number of items */
+        public GenericTestCollections(int length, GenerateElement<TKey, TValue> kvp_generator)
+        {
+            list_of_keys = new List<TKey>();
+            list_of_strings = new List<string>();
+            key_value_dict = new Dictionary<TKey, TValue>();
+            value_dict = new Dictionary<string, TValue>();
+
+            this.generate_element_method = kvp_generator;
+
+            for (int i = 0; i < length; i++)
+            {
+                /* Gnerating equal but different KVP objects */
+                KeyValuePair<TKey, TValue> kvp1 = generate_element_method(i);
+                KeyValuePair<TKey, TValue> kvp2 = generate_element_method(i);
+
+                list_of_keys.Add(kvp1.Key);
+                list_of_strings.Add(kvp1.Key.ToString());
+
+                key_value_dict.Add(kvp2.Key, kvp2.Value);
+                value_dict.Add(kvp1.Key.ToString(), kvp1.Value);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (TKey tk in list_of_keys)
+            {
+                s += tk.ToString();
+            }
+            s += "\n";
+
+            foreach (string tk in list_of_strings)
+            {
+                s += tk;
+            }
+            s += "\n";
+            foreach (KeyValuePair<TKey, TValue> kvp in key_value_dict)
+            {
+                s += kvp.ToString();
+            }
+            s += "\n";
+            foreach(KeyValuePair<string, TValue> kvp in value_dict)
+            {
+                s += kvp.ToString();
+            }
+            return s;
+        }
     }
 }
