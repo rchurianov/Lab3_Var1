02b1f7c6a07b510b6da458407e530e7444bec80d
rchurianov <rchurianov@gmail.com>
2017-03-01 13:31:47 +0300

    GenericTestCollections - added TimeComparison method

--- Lab3_Var1/GenericTestCollections.cs
+++ Lab3_Var1/GenericTestCollections.cs
@@ -39,6 +39,112 @@ namespace Lab3_Var1
             }
         }
 
+        /* Method to get search times for collections.
+         * Method returns an array of integers. Each element in the array is
+         * search time in ticks for a specific element in one of collections.
+         * [output_array_index] - description
+         * [0] - search time for the first element in List<TKey>
+         * [1] - search time for the central element in List<TKey>
+         * [2] - search time for the last element in List<TKey>
+         * [3] - search time for a non-existent element in List<TKey>
+         * [4] - search time for the first element in List<string>
+         * [5] - search time for the central element in List<string>
+         * [6] - search time for the last element in List<string>
+         * [7] - search time for the non-existent element in List<string>
+         * [8] - search time by key for the first element in Dictionary<TKey, TValue>
+         * [9] - search time by key for the central element in Dictionary<TKey, TValue>
+         * [10] - search time by key for the last element in Dictionary<TKey, TValue>
+         * [11] - search time by key for a non-existent element in Dictionary<TKey, TValue>
+         * [12] - search time by key for the first element in Dictionary<string, TValue>
+         * [13] - search time by key for the central element in Dictionary<string, TValue>
+         * [14] - search time by key for the last element in Dictionary<string, TValue>
+         * [15] - search time by key for a non-existent element in Dictionary<string, TValue>
+         * [16] - search time by value for the first element in Dictionary<TKey, TValue>
+         * [17] - search time by value for the central element in Dictionary<TKey, TValue>
+         * [18] - search time by value for the last element in Dictionary<TKey, TValue>
+         * [19] - search time by value for a non-existent element in Dictionary<TKey, TValue>
+         */
+
+        public int[] TimeComparison()
+        {
+            int[] results = new int[20];
+            int collections_length =  list_of_keys.Count;
+            int[] indexes = {0, collections_length / 2, collections_length-1, collections_length + 100};
+            int result_index = 0;
+
+            /* Searching for elements in list_of_keys */
+            foreach(int index in indexes)
+            {
+                KeyValuePair<TKey, TValue> kvp = generate_element_method(index);
+                int start_time = Environment.TickCount;
+                bool b = this.list_of_keys.Contains(kvp.Key);
+                results[result_index] = Environment.TickCount - start_time;
+                if (!b & index < collections_length + 100)
+                    Console.WriteLine("Could not find {0} element in list_of_keys.", index);
+                else if (b & index == collections_length + 100)
+                    Console.WriteLine("Found non-existent element in list_of_keys.");
+                result_index += 1;
+            }
+
+            /* Searching for elements in list_of_strings */
+            foreach (int index in indexes)
+            {
+                KeyValuePair<TKey, TValue> kvp = generate_element_method(index);
+                int start_time = Environment.TickCount;
+                bool b = this.list_of_strings.Contains(kvp.Key.ToString());
+                results[result_index] = Environment.TickCount - start_time;
+                if (!b & index < collections_length + 100)
+                    Console.WriteLine("Could not find {0} element in list_of_stings.", index);
+                else if (b & index == collections_length + 100)
+                    Console.WriteLine("Found non-existent element in list_of_strings.");
+                result_index += 1;
+            }
+
+            /* Searching for elements by Key in key_value_list */
+            foreach (int index in indexes)
+            {
+                KeyValuePair<TKey, TValue> kvp = generate_element_method(index);
+                int start_time = Environment.TickCount;
+                bool b = this.key_value_dict.ContainsKey(kvp.Key);
+                results[result_index] = Environment.TickCount - start_time;
+                if (!b & index < collections_length + 100)
+                    Console.WriteLine("Could not find {0} element in key_value_dict.", index);
+                else if (b & index == collections_length + 100)
+                    Console.WriteLine("Found non-existent element in key_value_dict.");
+                result_index += 1;
+            }
+
+            /* Searching for elements by Key in value_list */
+            foreach (int index in indexes)
+            {
+                KeyValuePair<TKey, TValue> kvp = generate_element_method(index);
+                int start_time = Environment.TickCount;
+                bool b = this.value_dict.ContainsKey(kvp.Key.ToString());
+                results[result_index] = Environment.TickCount - start_time;
+                if (!b & index < collections_length + 100)
+                    Console.WriteLine("Could not find {0} element in value_dict.", index);
+                else if (b & index == collections_length + 100)
+                    Console.WriteLine("Found non-existent element in value_dict.");
+                result_index += 1;
+            }
+
+            /* Searching for elements by Value in value_list */
+            foreach (int index in indexes)
+            {
+                KeyValuePair<TKey, TValue> kvp = generate_element_method(index);
+                int start_time = Environment.TickCount;
+                bool b = this.key_value_dict.ContainsValue(kvp.Value);
+                results[result_index] = Environment.TickCount - start_time;
+                if (!b & index < collections_length + 100)
+                    Console.WriteLine("Could not find {0} element in key_value_dict.", index);
+                else if (b & index == collections_length + 100)
+                    Console.WriteLine("Found non-existent element in key_value_dict.");
+                result_index += 1;
+            }
+
+            return results;
+        }
+
         public override string ToString()
         {
             string s = "";
